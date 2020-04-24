94e4667b43ee9811a153348b268229aee6d272db
broliang <18706703437@163.com>
2020-04-24 13:02:12 +0800

    Modify my_packages/my_macros.el: Use string instead of ascii code for macro.

--- my_packages/my_macros.el
+++ my_packages/my_macros.el
@@ -6,7 +6,9 @@
 
 ;;; Code:
 (fset 'my_macro_org_copy_agenda_link_line_to_journal_checklist
-   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([3 111 108 27 106 97 3 12 13 13 27 13 escape 27 107 106] 0 "%d")) arg)))
+   ;; (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([3 111 108 27 106 97 3 12 13 13 27 13 escape 27 107 106] 0 "%d")) arg)))
+   ;; Use (byte-to-string int) to translate
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("oljaxevil-normal-statekj" 0 "%d")) arg)))
 
 (fset 'my_macro_copy_all_agenda_items_link_to_journal
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("xhelm-modeoaaglocjk*xfmy_macro_org_copy_agenda_link_line_to_journal_checklistjddxhelm-mode" 0 "%d")) arg)))
