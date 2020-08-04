ea53be23bf1cfbc755728d75575646c5de145303
broliang <18706703437@163.com>
2020-08-04 22:54:39 +0800

    Add init.el: Add scala-mode to use-package.

--- init.el
+++ init.el
@@ -1875,12 +1875,32 @@
         ;; (setq lsp-log-io t)
   :hook
         (lsp-mode . lsp-enable-which-key-integration)
+        (scala-mode . lsp)
   )
 
 (use-package lsp-java
   :hook
         (java-mode . lsp)
   )
+
+(use-package scala-mode
+  :mode "\\.s\\(cala\\|bt\\)$"
+  )
+
+(use-package sbt-mode
+  :commands sbt-start sbt-command
+  :config
+        ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+        ;; allows using SPACE when in the minibuffer
+        (substitute-key-definition
+        'minibuffer-complete-word
+        'self-insert-command
+        minibuffer-local-completion-map)
+        ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
+        (setq sbt:program-options '("-Dsbt.supershell=false"))
+)
+
+(use-package lsp-metals)
 
 (use-package helm-lsp
   )
