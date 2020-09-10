8c326a7e877affc4842e39e8272370adec520ec2
broliang <18706703437@163.com>
2020-09-10 17:11:51 +0800

    Archive elpa

---
+++ elpa/helm-firefox-20200306.1408/helm-firefox-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-firefox" "20200306.1408" "Firefox bookmarks" '((helm "1.5") (cl-lib "0.5") (emacs "24.1")) :commit "7065e01188ed17b86a7b4f01b95ace575a15eef1" :url "https://github.com/emacs-helm/helm-firefox")
---
+++ elpa/org-download-20200506.1829/org-download-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-download" "20200506.1829" "Image drag-and-drop for Org-mode." '((emacs "24.3") (async "1.2")) :commit "d248fcb8f2592a40507682e91eed9a31ead4e4a6" :keywords '("multimedia" "images" "screenshots" "download") :authors '(("Oleh Krehel")) :maintainer '("Oleh Krehel") :url "https://github.com/abo-abo/org-download")
