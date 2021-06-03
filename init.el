0f021ac2c3b08f133539b7f9b2fe5fa2976ec7a6
broliang <18706703437@163.com>
2021-06-03 15:56:20 +0800

    Set up web-mode and tide.

--- init.el
+++ init.el
@@ -1989,6 +1989,31 @@
   :mode "\\.json\\'"
   )
 
+(use-package web-mode
+  :config
+        (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+        (add-hook 'web-mode-hook
+                (lambda ()
+                    (when (string-equal "tsx" (file-name-extension buffer-file-name))
+                    (setup-tide-mode))))
+        ;; enable typescript-tslint checker
+        (flycheck-add-mode 'typescript-tslint 'web-mode)
+  )
+
+(use-package tide
+  :after web-mode
+  :config
+        (tide-setup)
+        (flycheck-mode +1)
+        (setq flycheck-check-syntax-automatically '(save mode-enabled))
+        (eldoc-mode +1)
+        (tide-hl-identifier-mode +1)
+        (company-mode +1)
+        ;; formats the buffer before saving
+        (add-hook 'before-save-hook 'tide-format-before-save)
+        (add-hook 'typescript-mode-hook #'setup-tide-mode)
+  )
+
 (use-package json-reformat
   )
 
