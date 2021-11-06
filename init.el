a65ffdc50b7205970474913c324af8339bd1eb50
broliang <18706703437@163.com>
2021-11-06 18:13:58 +0800

    Fix setup for tide.

--- init.el
+++ init.el
@@ -2456,13 +2456,16 @@
 (use-package tide
 ;; TypeScript Interactive Development Environment for Emacs
   :after web-mode
+  :preface
+        (defun setup-tide-mode ()
+            (interactive)
+            (tide-setup)
+            (flycheck-mode +1)
+            (setq flycheck-check-syntax-automatically '(save mode-enabled))
+            (eldoc-mode +1)
+            (tide-hl-identifier-mode +1)
+            (company-mode +1))
   :config
-        (tide-setup)
-        (flycheck-mode +1)
-        (setq flycheck-check-syntax-automatically '(save mode-enabled))
-        (eldoc-mode +1)
-        (tide-hl-identifier-mode +1)
-        (company-mode +1)
         ;; formats the buffer before saving
         (add-hook 'before-save-hook 'tide-format-before-save)
         (add-hook 'typescript-mode-hook #'setup-tide-mode)
