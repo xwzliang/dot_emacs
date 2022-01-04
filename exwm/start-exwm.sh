b3297af614c23994d0ce4c129986a09e2ecb1e17
broliang <18706703437@163.com>
2022-01-04 18:42:50 +0800

    Setup screen lock for exwm.

--- exwm/start-exwm.sh
+++ exwm/start-exwm.sh
@@ -6,5 +6,8 @@ xrdb ~/git/dot_emacs/exwm/Xresources
 # Run the screen compositor
 picom &
 
+# Enable screen locking on suspend
+xss-lock -- slock &
+
 # Fire it up
 exec dbus-launch --exit-with-session emacs -mm --debug-init
